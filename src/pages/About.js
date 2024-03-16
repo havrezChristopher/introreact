3ce5ebefc815a78d33b92b2cf597779770867818
Havrez Christopher <havrez.christopher@outlook.com>
2024-03-16 01:21:36 +0100

    React == ( 1/2 )

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo></Logo>
+      <Navigation></Navigation>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quos
+        delectus libero eos distinctio possimus. Possimus aspernatur, enim
+        asperiores non eum, nostrum quibusdam quod veritatis quae odio, vero aut
+        repudiandae vitae eos. Eligendi eaque in excepturi nemo nihil corporis,
+        dolores tenetur aliquid rerum totam eius aperiam nesciunt provident
+        minus et eos omnis voluptate iusto nostrum expedita quaerat ipsam vero
+        illum? Accusantium eveniet nemo temporibus quisquam officia libero
+        similique veniam ipsum dicta perferendis, tenetur quae facilis inventore
+        aliquid culpa ab rerum illum doloribus veritatis, est eius! Esse
+        adipisci repellat odit qui quidem ipsum? Excepturi unde id minima nihil
+        maiores quasi non.
+      </p>
+    </div>
+  );
+};
+
+export default About;
