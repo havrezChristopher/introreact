58d85d4261ad269795efa6f2a596cf9f35416c3b
Havrez Christopher <havrez.christopher@outlook.com>
2024-03-24 22:14:20 +0100

    Vercel Analytique

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -7,6 +9,7 @@ import Blog from "./pages/Blog";
 function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Analytics } from "@vercel/analytics/react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.scss";
@@ -8,6 +7,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>
 );
